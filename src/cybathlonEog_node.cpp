7f50bbbe291b93143b1b7bc23cfab2fadc2a3c8e
Luca Tonin <luca.tonin@unipd.it>
2024-05-13 16:55:54 +0200

    Sending events only if there is artifact (at the neuroframe rate

--- src/cybathlonEog_node.cpp
+++ src/cybathlonEog_node.cpp
@@ -19,10 +19,8 @@ int main(int argc, char** argv) {
 	{
 		 if(eogbci.Apply() == true) {
 			ROS_INFO_ONCE("Eog detection started"); 
+			eogbci.HasArtifacts();
 		 }
-
-		eogbci.HasArtifacts();
-
          ros::spinOnce();
 		 r.sleep();
 	}
